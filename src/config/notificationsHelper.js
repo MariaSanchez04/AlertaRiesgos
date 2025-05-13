f460c8c828dab819ef3ffca3cdc74166aff5a42b
RuedaCristian15 <ruedacristian1512@gmail.com>
2025-05-12 19:15:29 -0500

    Create notificationsHelper.js

---
+++ src/config/notificationsHelper.js
@@ -0,0 +1,30 @@
+// src/utils/notificationsHelper.js
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+
+export async function configurarNotificaciones() {
+  const { status } = await Notifications.requestPermissionsAsync();
+  if (status !== "granted") {
+    console.log("Permiso de notificaciones denegado.");
+    return;
+  }
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("reportes", {
+      name: "Reportes",
+      importance: Notifications.AndroidImportance.HIGH,
+      sound: "default",
+    });
+  }
+}
+
+export async function mostrarNotificacion(titulo, cuerpo) {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: titulo,
+      body: cuerpo,
+      sound: "default",
+    },
+    trigger: null, // inmediata
+  });
+}
