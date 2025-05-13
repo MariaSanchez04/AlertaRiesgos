2a564aabfb0129d5c98feffacad14f664c07181a
RuedaCristian15 <ruedacristian1512@gmail.com>
2025-05-12 19:17:20 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator, Header } from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import HomeScreen from "./screens/HomeScreen";
 import TakePhotoAndUpload from "./screens/TakePhotoAndUpload";
 import ReportListScreen from "./screens/ReportListScreen";
+import ReporteDetalle from "./screens/ReporteDetalle"; // ✅ Asegúrate que la ruta y nombre del archivo sea correcto
 
 const Stack = createStackNavigator();
 
@@ -13,11 +14,33 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Takephoto" component={TakePhotoAndUpload} />
-        <Stack.Screen name="Reportes" component={ReportListScreen} />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Takephoto"
+          component={TakePhotoAndUpload}
+        />
+        <Stack.Screen
+          name="Reportes"
+          component={ReportListScreen}
+        />
+        <Stack.Screen
+          name="ReporteDetalle"
+          component={ReporteDetalle}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
